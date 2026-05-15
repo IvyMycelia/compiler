5eba83bf58c5cbd323ad6e8fea443be6bc0ca701
Ivy Banana <cnnorwood14@gmail.com>
2026-05-14 22:52:38 -0400

    Updated Makefile to be formatted better and build properly Updated build.sh to properly run and build Flower, save backups, and apply successful builds

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 set -e
 
-WORKING =   "./bin/Flower"
-BACKUP  =   "./bin/Flower_backup"
-NEW_C   =   "./bin/Flower_new.c"
-NEW_BIN =   "./bin/Flower_new"
+WORKING="./bin/Flower"
+BACKUP="./bin/Flower_backup"
+NEW_C="./bin/Flower_new.c"
+NEW_BIN="./bin/Flower_new_bin"
+TEST_C="./bin/Flower_test.c"
 
 echo "=== Building new version ==="
-$WORKING ./src/main.flo $NEW_C
+$WORKING ./src/main.flo ./bin/Flower_new
 
 echo "=== Compiling new binary ==="
 gcc $NEW_C -o $NEW_BIN -fsanitize=address -fno-omit-frame-pointer
 
 echo "=== Testing new compiler ==="
-$NEW_BIN ./src/main.flo ${NEW_C}.test
+$NEW_BIN ./src/main.flo ./bin/Flower_test
 
 if [ $? -ne 0 ]; then
     echo "ERROR: New compiler failed self-test!"
-    rm $NEW_C $NEW_BIN
+    rm -f $NEW_C $NEW_BIN $TEST_C
     exit 1
 fi
 
-echo "=== Check ==="
-diff $NEW_C ${NEW_C}.test > /dev/null
+echo "=== Idempotency check ==="
+diff $NEW_C $TEST_C > /dev/null
 if [ $? -ne 0 ]; then
     echo "WARNING: Generated code differs (may be ok)"
 fi
@@ -35,4 +36,4 @@ echo "Bootstrap successful"
 echo "Previous version backed up to: $BACKUP"
 
 # Cleanup
-rm -f $NEW_C ${NEW_C}.test $NEW_BIN
+rm -f $NEW_C $NEW_BIN $TEST_C
